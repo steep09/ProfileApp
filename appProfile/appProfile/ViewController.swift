ac4e0d59a72e40cc22281847fcd3576d5dcf32e5
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-07 12:17:34 +0800

    start of the project

---
+++ appProfile/appProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appProfile
+//
+//  Created by Stephenson Ang on 07/05/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
